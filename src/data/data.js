b97713f5be4ff4fe846667c05759bf96c3d790c6
Suhas738 <114868547+Suhas738@users.noreply.github.com>
2023-09-17 20:03:18 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Game from '../assets/work/game.jpg';
+import WorkIm from '../assets/work/WorkImg.png';
+import Dehaze from '../assets/work/dehazing.jpg';
+import Write from '../assets/work/handwritten.jpg';
+
+export const data=[
+    {
+        id:1,
+        name:"Solar System Simulator",
+        image:WorkIm,
+        github:"https://github.com/Suhas738/Solar-System-Simulator",
+        live:"",
+    },
+    {
+        id:2,
+        name:"2D-Game using pygame",
+        image:Game,
+        github:"https://github.com/Suhas738/2d-figthing-game-using-pygame",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Image Dehazing using ML",
+        image:Dehaze,
+        github:"https://github.com/Suhas738/Image-Dehazing-deployed-main",
+        live:"",
+    },
+    {
+        id:4,
+        name:"handwriten Character Recognition",
+        image:Write,
+        github:"https://github.com/Suhas738/Handwritten-Character-Recognition-using-ML",
+        live:"",
+    },
+
+
+]
